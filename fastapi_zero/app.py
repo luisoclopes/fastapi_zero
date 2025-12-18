a54220bc757f050b6cf8499a56ff0d4d3abf129f
Luis Otavio <luisomcanto@gmail.com>
2025-12-18 17:53:31 -0300

    Adiciona schemas e ajusta app inicial

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Minha primeira API', version='1.0.0')
 
 
-@app.get("/")
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Olá Mundo"}
+    return {'message': 'Olá Mundo!'}
